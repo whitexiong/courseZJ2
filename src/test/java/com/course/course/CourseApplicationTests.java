7c272009d2ca18db9f0c92340a5d9d077208186b
whitexiong <986247535@qq.com>
2021-04-11 05:29:05 +0800

    项目初始化 1. 注册中心完成 Eureka 2. system 完成 3. gateway 网关服务启动完成 修改了 spring-cloud-starter-netflix-eureka-server pom.xml

---
+++ src/test/java/com/course/course/CourseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.course.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
